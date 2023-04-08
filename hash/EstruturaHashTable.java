c85d64f24677628f85e656621e752dcbc9fbf353
PedroAlmeida18 <109418292+PedroAlmeida18@users.noreply.github.com>
2023-04-08 15:19:33 +0000

    Implentação do HASH

--- hash/EstruturaHashTable.java
+++ hash/EstruturaHashTable.java
@@ -1,4 +1,9 @@
 package hash;
+
+import java.util.ArrayList;
+
+import javax.swing.text.TabExpander;
+
 public class EstruturaHashTable implements EstruturaDeDados{
 
     private Integer tabela[];
@@ -9,22 +14,60 @@ public class EstruturaHashTable implements EstruturaDeDados{
 
     @Override
     public boolean insert(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, o elemento NÃO é inserido e retorna false. Caso contrário, o elemento é inserido na posição calculada e retorna true.
+            int posição= calcularposicação(chave);
+            if (tabela[posição] == null){
+                tabela[posição] =  chave;
+                return true;
+                }
+             else  {
+               if (tabela[posição].equals(chave)){
+                return false;
+               }    
+            }
+           
+        // TODO quando inserir, se a posição estiver ocupada,
+        // o elemento NÃO é inserido e retorna false. Caso contrário, o elemento é inserido na posição calculada e retorna true.
+
         return false;
-        
     }
 
     @Override
     public boolean delete(int chave) {
+        int posição= calcularposicação(chave);
+        if (tabela[posição]!= null && tabela[posição].equals(chave)){
+            tabela[posição] = null;
+            return true;
+        }
+        else {
+            return false;
+        }
         // TODO quando inserir, se a posição estiver ocupada, torna o elemento da posição como null e retorna true. Caso contrário, retorna false.
-        return false;
+        
         
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO se a posição estiver ocupada e valor é o mesmo (ou seja, tem que ser ambos), retorna true. Caso contrário, retorna true.
-        return false;
+        // TODO se a posição estiver ocupada e valor é o mesmo 
+        // (ou seja, tem que ser ambos), retorna true. Caso contrário, retorna true.
+        int posição = calcularposicação(chave);
+        if(tabela[posição] != null && tabela[posição].equals(chave)){
+                return true;
+
+        }
+        else {
+
+        
+            return false;
+
+        }
+        
+    }
+    public int  calcularposicação(int chave ){
+        int calculo = chave % 1000;
+        return calculo ;
     }
     
-}
+
+    }
+
